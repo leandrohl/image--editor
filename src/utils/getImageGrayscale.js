f9e8dbf8b21003df97a1e344dc9be2f43b63d3de
Leandro <leandro.silva@marttech.com.br>
2022-06-13 20:17:57 -0300

    fix: corretor

--- src/utils/getImageGrayscale.js
+++ src/utils/getImageGrayscale.js
@@ -4,11 +4,12 @@ export const getImageGrayscale = (imageData) => {
   let grayscaleData = []
 
   for(let i = 0; i < data.length; i+=4) {
-      gray = (data[i] + data[1] + data[2]) / 3
+      gray = data[i] !== data[i+1] || data[i] !== data[i+2]
+      ? Math.round((0.299*data[i] + 0.587*data[i+1] + 0.114*data[i+2]) / 3)
+      : data[i]
       grayscaleData[i]   = gray
       grayscaleData[i+1] = gray
       grayscaleData[i+2] = gray
-      grayscaleData[i+3] = 255
   }
 
   return grayscaleData
